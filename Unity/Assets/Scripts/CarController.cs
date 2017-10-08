a83d1b8b60b5be2cbfa374734fb5328fe88f5727
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-10-08 10:22:35 +0900

    Textのアニメーション

--- Unity/Assets/Scripts/CarController.cs
+++ Unity/Assets/Scripts/CarController.cs
@@ -10,6 +10,15 @@ public class CarController : MonoBehaviour {
 	void Start () {
 		WebSocketManager.Instance.Connect ("wss://taptappun.net/streaming");
 		WebSocketManager.Instance.OnReceiveMessage += OnReceiveMessage;
+        StartCoroutine(StayScroll());
+	}
+
+	IEnumerator StayScroll()
+	{
+		yield return new WaitForSeconds(3.0f);
+        mainUi.AnnounceRedTurn();
+		yield return new WaitForSeconds(10.0f);
+        mainUi.AnnounceBlueTurn();
 	}
 
 	public void OnReceiveMessage(string message){
--- Unity/Assets/Scripts/MainUI.cs
+++ Unity/Assets/Scripts/MainUI.cs
@@ -7,6 +7,44 @@ using Newtonsoft.Json;
 public class MainUI : MonoBehaviour {
 	[SerializeField] Slider redSlider;
 	[SerializeField] Slider blueSlider;
+	[SerializeField] Text turnText;
+    [SerializeField] float reachSecond = 1.0f;
+
+	public void AnnounceRedTurn(){
+		turnText.gameObject.SetActive (true);
+		turnText.color = Color.red;
+		turnText.text = "Red Turn";
+        StartCoroutine(ScrollTurnTextCorutine());
+	}
+
+	public void AnnounceBlueTurn(){
+		turnText.gameObject.SetActive (true);
+		turnText.color = Color.blue;
+		turnText.text = "Blue Turn";
+		StartCoroutine(ScrollTurnTextCorutine());
+	}
+
+    IEnumerator ScrollTurnTextCorutine(){
+        Vector3 rightPosition = new Vector3(this.GetComponent<RectTransform>().sizeDelta.x / 2 + turnText.rectTransform.sizeDelta.x, 0, 0);
+        float second = 0;
+        while(second < reachSecond){
+            turnText.transform.localPosition = Vector3.Lerp(rightPosition, Vector3.zero, second / reachSecond);
+			Debug.Log(turnText.transform.localPosition);
+            second += Time.deltaTime;
+			yield return null;
+        }
+        yield return new WaitForSeconds(1.0f);
+        Vector3 leftPosition = new Vector3(-rightPosition.x, 0 , 0);
+		Debug.Log(leftPosition);
+		second = 0;
+		while (second < reachSecond)
+		{
+            turnText.transform.localPosition = Vector3.Lerp(Vector3.zero, leftPosition, second / reachSecond);
+			second += Time.deltaTime;
+			yield return null;
+		}
+        turnText.gameObject.SetActive(false);
+    }
 
 	public void ChangeRedSlider(string message){
 		Debug.Log (message);
